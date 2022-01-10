fa81e7a97d6d5f66a020baaee3776d8046f4a80e
vidhu <vidhu.v9@gmail.com>
2022-01-10 20:54:49 +0530

    intial commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+
+export const projects = [
+    {
+      title: "Covidify -19",
+      subtitle: "React, npm packages",
+      description:
+        "Developed an textbf UI which shows daily and total covid cases, recoveries and deaths country wise and worldwide",
+      // image: "./project-1.gif",
+      image: "./covid.png",
+      link: "https://vidhu007.github.io/covid-tracker/",
+    },
+    {
+        title: "Dynamic Dictionary",
+        subtitle: "React, npm packages",
+        description:
+          "Developed an dynamic dictionary which gives meanings dynamically as the user types anything and also gives audio pronounciation of the typed words",
+        // image: "./project-1.gif",
+        image: "./dict.png",
+        link: "https://vidhu007.github.io/React-Dictionary/",
+      },
+    {
+        title: "URL- shortener",
+        subtitle: "HTML,CSS,NodeJS,ExpressJS,MongoDB",
+        description:
+          "Developed a web app which shortens any URL that the user types",
+        // image: "./project-1.gif",
+        image: "./link.png",
+        link: "https://vidhu007.github.io/covid-tracker/",
+      },
+
+  ];
+  
+  export const skills = [
+"C++", "C", "Javascript","Python","HTML3","CSS3", "React","NodeJS", "ExpressJS","MongoDB","SQL", "Git & Github"
+
+  ];
+
+  export const testimonials = [
+      {
+      namee: "Kim Hyun Suk",
+      image: "https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco,dpr_1/ljdlnzi5jqabbm2g7bf5",
+      quote: "Vidhu worked as a R & D intern here at my company Samsung. He is a very hard working guy and performed really well in the 2 months I could intereact with him. He always gives his 100% at any situation!",
+      company: "Samsung Research Institute Banglore"
+      }
+  ]
